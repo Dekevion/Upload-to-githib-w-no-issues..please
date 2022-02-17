cbc949899d3869acaaae8f7cab9ccbedbb58ecd4
Dekevion <dekevionwade@yahoo.com>
2022-02-16 20:54:36 -0600

    Third Commit

--- plz_upload_to_github.js
+++ plz_upload_to_github.js
@@ -1,2 +1,17 @@
 console.log("Hello_Darkness_My_Old_Programmer");
 console.log("Second push");
+// Comments on how to push to Github repo.
+
+// Step 1: Make sure ur in the correct directory
+
+// Step 2: initialize directory press "git init" 
+//then hit return in terminal
+
+//Step 3: Stage file "git add ." 
+// git status to see files
+
+// Step 4: "git commit -m"  "commit comment"
+
+//step 5: git remote add origin github url
+
+// Step 6 "git push"
