1a45e482f37cbd2dc404dbcea0683890ace7103f
Dekevion <dekevionwade@yahoo.com>
2022-02-17 07:54:05 -0600

    Statements

---
+++ Tempconversion.js
@@ -0,0 +1,25 @@
+// let farhrenheit = 32;
+// let kelvin = 300;
+
+// let celsius_formula = (farhrenheit - 32) * 5/9;
+// console.log(celsius_formula);
+
+// let kelvin_formula = (farhrenheit +  459.67) * 5/9;
+// console.log(kelvin_formula);
+
+
+// Booleans
+// === equality
+// !== not equal
+// <
+// >
+// <=
+// >= 
+let age = 23;
+
+let isChild = age <= 7;
+
+let isSenior = age >= 65;
+
+console.log(age, isChild, isSenior);
+
---
+++ ifStatements.js
@@ -0,0 +1,17 @@
+let temp = 110
+
+// let isFreezing = temp <= 32;
+
+// if ( temp <= 32) {
+//     console.log("it is freezing outside")
+// } else {
+//     console.log("its hot outside")
+// };
+
+if (temp < 45) {
+    console.log("It is cold outside")
+} else if (temp >= 110) {
+    console.log("it is hot outside")
+} else {
+    console.log("its ok out here")
+}
