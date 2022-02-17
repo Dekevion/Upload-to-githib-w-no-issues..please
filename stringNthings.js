1834786e1ffa4bae2d0dc317f754305ba5edfcab
Dekevion <dekevionwade@yahoo.com>
2022-02-16 21:08:54 -0600

    String commit

---
+++ stringNthings.js
@@ -0,0 +1,11 @@
+var first_name = "DeKevion";
+let last_name = "Wade";
+let fullName = first_name + " " + last_name;
+
+var city = "Memphis";
+var country = "Tn."
+var location = city + "," + country;
+
+console.log(fullName);
+
+console.log(location);
